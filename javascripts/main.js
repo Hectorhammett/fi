f337a149f67d64e443619d3561788c9e935f8155
Hector Mendoza <mendozah@localhost.localdomain>
2016-07-11 09:55:16 -0700

    -Added the scroll js

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,3 +9,24 @@ $(window).scroll(function() { // check if scroll event happened
 $('.navbar-toggle').click(function(){
   $(".navbar-fixed-top").toggleClass("clicked-burger");
 })
+
+$("a").on('click', function(event) {
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+  
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  } // End if
+});
