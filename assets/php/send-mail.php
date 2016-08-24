e4099ea7a1a3b43f4143a3b10be1fbced5d6794b
Hector Mendoza <mendozah@localhost.localdomain>
2016-08-23 20:18:04 -0700

    Send Mail test

--- assets/php/send-mail.php
+++ assets/php/send-mail.php
@@ -1,22 +1,21 @@
-<?php
-    var_dump($_POST);
-// if(isset($_POST)){
-//     $to = "hector.mendoza@mitechnologiesinc.com"; // this is your Email address
-//     $from = $_POST['from']; // this is the sender's Email address
-//     $name = $_POST['clientname'];
-//     $subject = $_POST['subject'];
-//     $subject2 = "Copy of your form submission";
-//     $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-//     $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+<?php 
+if(isset($_POST["clientname"])){
+    $to = "hector.mendoza@mitechnologiesinc.com"; // this is your Email address
+    $from = $_POST['from']; // this is the sender's Email address
+    $name = $_POST['clientname'];
+    $subject = $_POST['subject'];
+    $subject2 = "Copy of your form submission";
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
-//     $headers = "From:" . $from;
-//     $headers2 = "From:" . $to;
-//     mail($to,$subject,$message,$headers);
-//     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-//     echo 'Thank you! We will be in touch soon.';
-// }
-// else{
-//     header("Location: https://www.fininsite.com"); /* Redirect browser */
-//     exit();
-// }
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo 'Thank you! We will be in touch soon.';
+}
+else{
+    header("Location: https://www.fininsite.com"); /* Redirect browser */
+    exit();
+}
 ?>
