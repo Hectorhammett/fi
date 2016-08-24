b423dbbae95263694e1a47d88060a7303b25b5f3
Hector Mendoza <mendozah@localhost.localdomain>
2016-08-23 20:12:59 -0700

    Send Mail test

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,6 @@
 $("#form-email").submit(function(e){
   e.preventDefault();
-  $.post('',function(response){
+  $.post('https://www.fininsite.com/wp-content/themes/fininsite/assets/php/send-mail.php',$(this).serialize(),function(response){
     swal("Thank You!", "We will be in touch soon.");
   })
   .fail(function(response){
